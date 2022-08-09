88823faf92350df83fe67f5353fe3d9f73fd12e2
ItDev-Angel <itdevangel@gmail.com>
2022-08-09 20:38:59 +0300

    2. Знакомство с JSX

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-const h1 = <h1>Hello world</h1>
+const name = 'Иван-Царевич';
+function formatName(user:any) {
+  return user.firstName + ' ' + user.lastName;
+}
+const user = {
+  firstName: 'Марья',
+  lastName: 'Моревна',
+  avatarUrl: 'https://i.pinimg.com/736x/94/bb/6e/94bb6e2ba93089cba8b60e6d3ca7c00e.jpg'
+};
+const LinkUrl = <a href="https://www.reactjs.org"> link </a>;
+const Img = <img src={user.avatarUrl} alt="img" width="200"/>;
+const element = <h1>Здравствуй!, {name}, {formatName(user)}</h1>;
+function getGreeting(user:any) {
+  if (!user) {
+    return <h1>Здравствуй, {formatName(name)}!</h1>;
+  }
+  return <h1>Здравствуй, незнакомец.</h1>;
+}
+const tag = (
+  <div>
+    <h1>Здравствуйте!</h1>
+    <h2>Рады вас видеть.</h2>
+  </div>
+);
+const title = user.firstName;
+// Этот код безопасен:
+const text = <h1>{title}</h1>;
+const greeting = (
+  <h1 className="greeting">
+    Привет, happy!
+  </h1>
+);
+const gree = React.createElement(
+  'h1',
+  {className: 'greeting'},
+  'Привет, мир!'
+);
+// Примечание: этот код несколько упрощён.
+const simple = {
+  type: 'h1',
+  props: {
+    className: 'greeting',
+    children: 'Привет, мирok!'
+  }
+};
 function App () {
   return (
     <div>
-      {h1}
+      <h1>{simple.props.children}</h1>
+      {gree}
+      {greeting}
+      {text}
+      {tag}
+      {LinkUrl}
+      {Img}
+      {element}
+      {getGreeting(user)}
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique, assumenda?</p>
     </div>
   )
@@ -13,5 +64,7 @@ function App () {
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<App />);
-
+root.render(
+  <> <App />
+  </>
+);
