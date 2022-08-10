f6f8bf5e79f03e79ef82613f8292a3176bbfccc6
ItDev-Angel <itdevangel@gmail.com>
2022-08-10 20:44:24 +0300

    header

---
+++ src/container/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CssBaseline from '@mui/material/CssBaseline';
+import Header from 'container/Header/Header';
+const App = () => {
+    return (
+        <>
+            <CssBaseline />
+            <Header />
+        </>
+    )
+}
+
+export default App
