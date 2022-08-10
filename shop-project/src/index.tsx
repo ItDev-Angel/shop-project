1c912f10f6752b6029ff5ba08a123664d8b74b79
ItDev-Angel <itdevangel@gmail.com>
2022-08-10 18:55:59 +0300

    react fragment

--- shop-project/src/index.tsx
+++ shop-project/src/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
-
+function App () {
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique, assumenda?</p>
+    </div>
+  )
+}
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render("Hello world");
+root.render(
+    <App />
+);
+
+
 
