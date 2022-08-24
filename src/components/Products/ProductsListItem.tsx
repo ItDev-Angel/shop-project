fdc3de9dd163e812a14b296c0d0db6b4abb18380
ItDev-Angel <itdevangel@gmail.com>
2022-08-24 19:38:15 +0300

    color change

--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -1,3 +1,4 @@
+import React, {useState} from 'react'
 import { Button, Card, CardActions, CardContent, TextField } from '@mui/material'
 import './ProductsListItem.scss'
 
@@ -13,23 +14,29 @@ export type PropsProduct = {
 }
 
 const ProductsListItem = ({name,image, description, type, capacity, price}: PropsProduct) => {
-
+    const [count, setCount] = useState<number>(1)
+    const [color, setColor] = useState<string>("red")
+    const onChangeClick = () => setColor((prevState:string)=>(prevState === "green"?"red":"green"))
+    const onIncrementClick = () => setCount((prevState:number)=>prevState +1)
+    const onDecrementClick = () => setCount((prevState:number)=>prevState -1)
     return (
         <Card>
             <CardContent>
                 <div className="product-img">
                     <img src={image} alt="" />
                 </div>
-                <h4 className="product-title">{name}</h4>
+                <h4 className={`product-title ${color}`}>{name}</h4>
                 <div className="product-description">{description}</div>
                 <div className="product-features">{capacity}</div>
                 <div className={`product-features`}>{type}</div>
                 <div className="product-price">{price}</div>
                 <div className="product-quantity">
-                    <Button variant="contained">-</Button>
-                    <TextField size="small" value={1} variant="outlined"/>
-                    <Button variant="contained">+</Button>
+                    <Button variant="contained" onClick={onDecrementClick}>-</Button>
+                    <TextField size="small" value={count} variant="outlined"/>
+                    <Button variant="contained" onClick={onIncrementClick}>+</Button>
                 </div>
+                <p>Color: {color}</p>
+                <Button onClick={onChangeClick}>Change color</Button>
             </CardContent>
 
             <CardActions className="btn-wraper">
